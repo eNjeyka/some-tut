e02da33fe4df4e85d24626027c6d251d27c8f703
Max Jey <enjeyka@yahoo.com>
2022-09-14 12:23:26 +0300

    работа со строкой

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 // стрелочная callback функция через 5с
-setTimeout(() => {
-    console.log("Hello");
-}, 5000);
+function lesson1 (text="hello", delay=5000) {
+    setTimeout(() => {
+        console.log(text);
+    }, delay);
+}
+// lesson1()
+
+// Создать переменную "Good Morning"
+// Заменить "Morning" на "Evening" и присвоить результат новой переменной
+// Вывести новую переменную в консоль
+function lesson2 () {
+    let gm = "Good Morning"
+    // находим индекс пробела
+    let spaceIndex = gm.indexOf(" ")
+    // создаем новую переменную для "Good Evening"
+    let ge = gm.slice(0, spaceIndex) + " Evening"
+    console.log(ge)
+}
+lesson2()
